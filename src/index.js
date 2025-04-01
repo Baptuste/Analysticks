1504d6df3a4eb83776619c715bdbd4556a58bd27
Baptuste <beatsse.games@gmail.com>
2025-04-02 00:04:41 +0100

    Fix: Résolution des conflits dans index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import './index.css'; // Assure-toi que ce fichier existe pour les styles globaux (même vide)
-import App from './App';
-=======
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
->>>>>>> 21d725322ed87b4dd76bf589532583160b76a8ba
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,11 +10,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-=======
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
->>>>>>> 21d725322ed87b4dd76bf589532583160b76a8ba
